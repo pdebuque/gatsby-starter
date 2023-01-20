d3728810f09f06d9dcfca2c059ad5a5095546d8b
Paolo Debuque <pdebuque@gmail.com>
2023-01-20 15:12:56 -0600

    boilerplate and about page

---
+++ src/pages/About.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function About() {
+  return (
+    <div>
+      <h1>About</h1>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quod fugit eos aspernatur dignissimos consequuntur explicabo autem, cumque voluptates rerum alias harum minus libero possimus animi dolor. Repudiandae officiis earum rem.
+      Optio voluptate harum, ducimus mollitia facilis odio inventore debitis quia facere, minima ad. Sint, veritatis culpa labore ut, excepturi obcaecati aperiam harum laboriosam eaque minus itaque officiis nulla praesentium laudantium!
+      Numquam voluptate ipsa blanditiis, quidem repudiandae itaque commodi praesentium qui, mollitia ratione harum dolore quia autem assumenda. Dolor accusantium, quaerat quis repellendus fuga adipisci aliquam illo possimus aperiam error quibusdam.
+      Maxime quis, ab repellat aut sunt temporibus quasi, enim rem quidem autem reiciendis eveniet repudiandae placeat nam nobis voluptatibus. Omnis incidunt sapiente ea, eum nostrum odio non ut asperiores impedit.
+      Maiores vitae adipisci, repudiandae excepturi provident animi voluptatem quos impedit dignissimos sapiente! Consequuntur, est iure incidunt sed velit porro. Neque adipisci voluptatum quis vero! Harum exercitationem incidunt eligendi natus inventore?</p>
+    </div>
+  )
+}
